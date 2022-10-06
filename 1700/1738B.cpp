9e8915f4e03e0c17f7d1597eb0a9c9de468a23c9
tingriley <handsomepot@gmail.com>
2022-10-06 14:23:10 +0800

    1738B

--- 1700/1738B.cpp
+++ 1700/1738B.cpp
@@ -1,9 +1,47 @@
 // 1738 B. Prefix Sum Addicts
 #include <bits/stdc++.h>
 using namespace std;
-
-int main()
+void solve() 
 {
-    printf("ok\n");
+    int n, k;
+    cin >> n >> k;
+    vector<long long> s(n + 1);
+    vector<long long> a(n + 1);
+    
+    
+    for (int i = n - k + 1; i <= n; i++) {
+        cin >> s[i];
+    }
+    
+    if (k == 1) {
+        cout << "YES" << endl;
+        return;
+    }
+
+    for (int i = n - k + 2; i <= n; i++) {
+        a[i] = s[i] - s[i - 1];
+    }
+
+    if (!std::is_sorted(a.begin() + n - k + 2, a.end())) {
+
+        cout << "NO" << endl;
+        return;
+    }
+    if (s[n - k + 1] > a[n - k + 2] * (n - k + 1)) {
+        cout << "NO" << endl;
+        return;
+    }
+
+    cout << "YES" << endl;
+}
+
+int main() 
+{
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
     return 0;
 }
