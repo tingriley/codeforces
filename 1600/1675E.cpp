0eb0493d15a0f336c813868816d8c0080d732253
tingriley <handsomepot@gmail.com>
2022-10-14 10:31:49 +0800

    1675E

--- 1600/1675E.cpp
+++ 1600/1675E.cpp
@@ -1,26 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){ 
-    int t,n,k;
+int main() {
+    int t, n, k;
     string s;
     cin >> t;
-    while(t--){
+    while (t--) {
         cin >> n >> k;
         cin >> s;
         int mp[256] = {};
-        for(int i = 0;i < n; i++){
-            while(s[i] > 'a'){
-                if(mp[s[i]])s[i]--;
-                else if(k){
+        for (int i = 0; i < n; i++) {
+            while (s[i] > 'a') {
+                if (mp[s[i]])
+                    s[i]--;
+                else if (k) {
                     k--;
                     mp[s[i]] = 1;
                     s[i]--;
-                }
-                else{
+                } else {
                     break;
                 }
             }
         }
-        cout<<s<<endl;
+        cout << s << endl;
     }
 }
