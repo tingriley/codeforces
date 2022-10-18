2f361dfe0d1ad3258372d09f6076f58bea8e21c2
tingriley <handsomepot@gmail.com>
2022-10-18 14:14:04 +0800

    1728C

--- 1700/1728C.cpp
+++ 1700/1728C.cpp
@@ -2,42 +2,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 void solve() {
-    int n; cin >> n;
-    priority_queue<int>pa;
-    priority_queue<int>pb;
+    int n;
+    cin >> n;
+    priority_queue<int> pa;
+    priority_queue<int> pb;
 
     int ans = 0;
 
-    for(int i = 0; i < n; i++){
-        int x; cin >> x;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
         pa.push(x);
     }
 
-    for(int i = 0; i < n; i++){
-        int x; cin >> x;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
         pb.push(x);
     }
-    while(!pa.empty() && !pb.empty()){
+    while (!pa.empty() && !pb.empty()) {
         int x1 = pa.top();
         int x2 = pb.top();
-        if(x1 == x2){
+        if (x1 == x2) {
             pa.pop();
             pb.pop();
-        }
-        else if(x1 > x2){
+        } else if (x1 > x2) {
             ans++;
             pa.push(to_string(pa.top()).size());
             pa.pop();
-        }
-        else if(x1 < x2){
+        } else if (x1 < x2) {
             ans++;
             pb.push(to_string(pb.top()).size());
             pb.pop();
         }
     }
-   
+
     cout << ans << endl;
 }
 int main() {
